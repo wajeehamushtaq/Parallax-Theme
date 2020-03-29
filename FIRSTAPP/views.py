290f49fa966d99f91c84aa0f5a2d13be0a7d9247
Wajeeha <wajeehamushtaq1@gmial.com>
2020-03-29 20:55:40 +0500

    Completed Task

---
+++ FIRSTAPP/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def hi(request):
+    return render(request,'FIRSTAPP/hi.html')
+
+
+
+
+
+
+
